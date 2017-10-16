41263a28e151a8fcf817e25f36d439233d3d07b0
Khai Do <zaro0508@gmail.com>
2017-10-16 16:41:01 -0700

    move aws cli setup into a bash script

--- interpolate_env_vars.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# double interpolate environment variables
-eval export "TravisUserKey=\$TravisUserKey_$TRAVIS_BRANCH"
-eval export "TravisUserSecretKey=\$TravisUserSecretKey_$TRAVIS_BRANCH"
-eval export "JacobianUserPassword=\$JacobianUserPassword_$TRAVIS_BRANCH"
