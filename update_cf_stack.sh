7d641dda55243fa06971a37c482537ed7977a5aa
Khai Do <zaro0508@gmail.com>
2017-10-30 22:38:51 -0700

    Add IAM user khai do
    Manage users and groups in CF.  Setup users with an initial
    password that will need to be changed on first login.

--- update_cf_stack.sh
+++ update_cf_stack.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # double interpolate environment variables
+eval export "InitNewUserPassword=\$InitNewUserPassword_$TRAVIS_BRANCH"
 eval export "JacobianUserPassword=\$JacobianUserPassword_$TRAVIS_BRANCH"
 eval export "NewRelicAccountId=\$NewRelicAccountId_$TRAVIS_BRANCH"
 eval export "NewRelicExternalId=\$NewRelicExternalId_$TRAVIS_BRANCH"
@@ -11,6 +12,7 @@ aws cloudformation update-stack \
 --capabilities CAPABILITY_NAMED_IAM \
 --template-body file://cf_templates/bridge.yml \
 --parameters \
+ParameterKey=InitNewUserPassword,ParameterValue="$InitNewUserPassword" \
 ParameterKey=JacobianUserPassword,ParameterValue="$JacobianUserPassword" \
 ParameterKey=NewRelicAccountId,ParameterValue=$NewRelicAccountId \
 ParameterKey=NewRelicExternalId,ParameterValue=$NewRelicExternalId
