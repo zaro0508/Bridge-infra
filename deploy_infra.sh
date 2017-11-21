59a589c589e60670c8e5cd40e5c968a3eeebb6d7
Khai Do <zaro0508@gmail.com>
2017-11-21 15:00:08 -0800

    Validate CF template on PR
    Currently Travis does not run a build when there is a PR. This change
    allows us turn on CF template validation when a PR is submitted.
    Deployment of the CF template will only occur when the PR is merge or
    when there is a direct push.

--- deploy_infra.sh
+++ deploy_infra.sh
@@ -4,13 +4,16 @@
 echo "Validating cloudformation templates..."
 aws cloudformation validate-template --template-body file://cf_templates/bridge.yml
 
-# Handle message that shouldn't be an error, https://github.com/hashicorp/terraform/issues/5653
-echo "Deploy cloudformation templates..."
-message=$(./update_cf_stack.sh 2>&1 1>/dev/null)
-error_code=$(echo $?)
-if [[ $error_code -ne 0 && $message =~ .*"No updates are to be performed".* ]]; then
-  echo "No stack changes detected. An update is not required."
-  exit 0
+# Deploy cloudformation template when PR merges or on a direct push
+if [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
+    echo "Deploy cloudformation templates..."
+    # Handle message that shouldn't be an error, https://github.com/hashicorp/terraform/issues/5653
+    message=$(./update_cf_stack.sh 2>&1 1>/dev/null)
+    error_code=$(echo $?)
+    if [[ $error_code -ne 0 && $message =~ .*"No updates are to be performed".* ]]; then
+      echo "No stack changes detected. An update is not required."
+      exit 0
+    fi
+    echo $message
+    exit $error_code
 fi
-echo $message
-exit $error_code
