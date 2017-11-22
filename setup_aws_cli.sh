7858271bb52424b51c2ffbdaa2f1c318fa71940b
Khai Do <zaro0508@gmail.com>
2017-11-21 16:17:17 -0800

    Normalize travis access key env var name
    The travis access key is used by multiple projects to deploy CF
    templates.  Set the ENV Var to the same name to make it easier
    to manage.

--- setup_aws_cli.sh
+++ setup_aws_cli.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # double interpolate vars from travis
-eval export "TravisUserKey=\$TravisUserKey_$TRAVIS_BRANCH"
-eval export "TravisUserSecretKey=\$TravisUserSecretKey_$TRAVIS_BRANCH"
+eval export "AwsTravisAccessKey=\$AwsTravisAccessKey_$TRAVIS_BRANCH"
+eval export "AwsTravisSecretAccessKey=\$AwsTravisSecretAccessKey_$TRAVIS_BRANCH"
 
 pip install --upgrade awscli
 mkdir ~/.aws
 echo -e "[default]\nregion=us-east-1" > ~/.aws/config
-echo -e "[default]\naws_access_key_id=$TravisUserKey\naws_secret_access_key=$TravisUserSecretKey" > ~/.aws/credentials
+echo -e "[default]\naws_access_key_id=$AwsTravisAccessKey\naws_secret_access_key=$AwsTravisSecretAccessKey" > ~/.aws/credentials
