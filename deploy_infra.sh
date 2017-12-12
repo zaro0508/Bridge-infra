692525173cff7956f5d20b8f6d6f455284946b5b
Khai Do <zaro0508@gmail.com>
2017-12-11 21:09:10 -0800

    refactor deploy scripts and decouple normalization from update

--- deploy_infra.sh
+++ deploy_infra.sh
@@ -3,20 +3,22 @@
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account')
 
 S3_TARGET_DIR="s3://bridge-cloudformation-artifacts-$AWS_ACCOUNT_ID/$STACK_NAME/"
-echo "Uploading cf_templates to $S3_TARGET_DIR"
+echo -e "\nUploading cf_templates to $S3_TARGET_DIR"
 aws s3 cp --recursive cf_templates $S3_TARGET_DIR
 
 TEMPLATE_URL="https://s3.amazonaws.com/bridge-cloudformation-artifacts-$AWS_ACCOUNT_ID/$STACK_NAME/bridge.yml"
-echo "Validating CF template $TEMPLATE_URL"
+echo -e "\nValidating CF template $TEMPLATE_URL"
 aws cloudformation validate-template --template-url $TEMPLATE_URL
 
-echo "Deploying CF template $TEMPLATE_URL"
+echo -e "\nDeploying CF template $TEMPLATE_URL"
 # Handle message that shouldn't be an error, https://github.com/hashicorp/terraform/issues/5653
-message=$(./update_cf_stack.sh $TEMPLATE_URL 2>&1 1>/dev/null)
+UPDATE_CMD="./update_cf_stack.sh $TEMPLATE_URL"
+message=$($UPDATE_CMD 2>&1 1>/dev/null)
 error_code=$(echo $?)
 if [[ $error_code -ne 0 && $message =~ .*"No updates are to be performed".* ]]; then
-  echo "No stack changes detected. An update is not required."
-  exit 0
+  echo -e "\nNo stack changes detected. An update is not required."
+  error_code=0
+else
+  echo $message
 fi
-echo $message
 exit $error_code
--- update_cf_stack.sh
+++ update_cf_stack.sh
@@ -2,14 +2,6 @@
 
 TEMPLATE_URL=$1
 
-# double interpolate environment variables
-eval export "InitNewUserPassword=\$InitNewUserPassword_$TRAVIS_BRANCH"
-eval export "JacobianUserPassword=\$JacobianUserPassword_$TRAVIS_BRANCH"
-eval export "NewRelicAccountId=\$NewRelicAccountId_$TRAVIS_BRANCH"
-eval export "NewRelicExternalId=\$NewRelicExternalId_$TRAVIS_BRANCH"
-eval export "OperatorEmail=\$OperatorEmail_$TRAVIS_BRANCH"
-
-# deploy with evaluated vars
 aws cloudformation update-stack \
 --stack-name $STACK_NAME \
 --capabilities CAPABILITY_NAMED_IAM \
