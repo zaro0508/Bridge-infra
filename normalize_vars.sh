16b1eceff409fa8caafaf0c31a9973370e8e1590
Khai Do <zaro0508@gmail.com>
2018-01-31 17:11:12 -0800

    WIP: setup a new vpc

--- normalize_vars.sh
+++ normalize_vars.sh
@@ -2,6 +2,8 @@
 
 # normalize env vars with by git branch
 eval export "AwsAccount=\$TRAVIS_BRANCH"
+eval export "AwsDefaultVpcId=\$AwsDefaultVpcId_$TRAVIS_BRANCH"
+eval export "AwsVpcSubnetPrefix=\$AwsVpcSubnetPrefix_$TRAVIS_BRANCH"
 eval export "CloudformationNotifyLambdaTopicArn=\$CloudformationNotifyLambdaTopicArn_$TRAVIS_BRANCH"
 eval export "InitNewUserPassword=\$InitNewUserPassword_$TRAVIS_BRANCH"
 eval export "JacobianUserPassword=\$JacobianUserPassword_$TRAVIS_BRANCH"
@@ -9,3 +11,4 @@ eval export "NewRelicAccountId=\$NewRelicAccountId_$TRAVIS_BRANCH"
 eval export "NewRelicExternalId=\$NewRelicExternalId_$TRAVIS_BRANCH"
 eval export "OperatorEmail=\$OperatorEmail_$TRAVIS_BRANCH"
 eval export "FhcrcVpnCidrip=\$FhcrcVpnCidrip_$TRAVIS_BRANCH"
+eval export "SSLCertArn=\$SSLCertArn_$TRAVIS_BRANCH"
